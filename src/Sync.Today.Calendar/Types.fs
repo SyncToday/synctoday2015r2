e89b12d0d20320f83b97c65ccdec32a00a32fda8
davidpodhola <david.podhola@naseukoly.cz>
2015-08-26 17:03:50 +0200

    reformatted

---
+++ src/Sync.Today.Calendar/Types.fs
@@ -0,0 +1,21 @@
+﻿namespace Sync.Today.Calendar
+
+open System
+
+[<CLIMutable>]
+type AppointmentDTO =
+    {   AppointmentId : int
+        InternalId : Guid
+        LastModified : DateTime
+        Category : string option
+        Location : string option
+        Content : string option
+        Title : string option
+        DateFrom: DateTime
+        DateTo: DateTime
+        ReminderMinutesBeforeStart: int
+        Notification : bool
+        IsPrivate : bool
+        Priority : byte
+        ConsumerId : int
+    }
