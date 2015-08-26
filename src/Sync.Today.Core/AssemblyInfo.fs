07894b100c87c8085af8ad352914a08d1c613b80
davidpodhola <david.podhola@naseukoly.cz>
2015-08-26 16:08:22 +0200

    Sync.Today.Core started

---
+++ src/Sync.Today.Core/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Sync.Today.Core")>]
+[<assembly: AssemblyProductAttribute("SyncToday2015R2")>]
+[<assembly: AssemblyDescriptionAttribute("Business processes automation platform")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
