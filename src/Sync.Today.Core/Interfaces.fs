d562498e8175e0275b0897867a49ab88e4281a59
davidpodhola <david.podhola@naseukoly.cz>
2015-08-26 16:26:15 +0200

    Interfaces started

---
+++ src/Sync.Today.Core/Interfaces.fs
@@ -0,0 +1,24 @@
+﻿namespace Sync.Today.Core
+
+module Interfaces =
+
+    /// Simple named item
+    ///
+    type INamed =
+        abstract member Name : string with get
+
+    /// Application e.g. Exchange, Google, SAP BusinessOne, Flores or Microsoft Dynamics CRM
+    ///
+    type IService =
+        inherit( INamed )
+
+    /// Generally known entity or entity combination e.g. Appointments, Products or Firms
+    ///
+    type IAdapter =
+        inherit( INamed )
+
+    /// An user of an application
+    ///
+    type IAccount = 
+        inherit( INamed )
+
