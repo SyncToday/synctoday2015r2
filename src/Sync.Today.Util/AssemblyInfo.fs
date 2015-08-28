a5f4fd326d4698fb18d1636a99ab83e2697ecc4d
davidpodhola <david.podhola@naseukoly.cz>
2015-08-28 14:29:51 +0200

    framework and F# vers fixed

--- src/Sync.Today.Util/AssemblyInfo.fs
+++ src/Sync.Today.Util/AssemblyInfo.fs
@@ -1,41 +1,12 @@
-﻿namespace Sync.Today.Util.AssemblyInfo
-
+﻿namespace System
 open System.Reflection
-open System.Runtime.CompilerServices
-open System.Runtime.InteropServices
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[<assembly: AssemblyTitle("Sync.Today.Util")>]
-[<assembly: AssemblyDescription("")>]
-[<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("Sync.Today.Util")>]
-[<assembly: AssemblyCopyright("Copyright ©  2015")>]
-[<assembly: AssemblyTrademark("")>]
-[<assembly: AssemblyCulture("")>]
+[<assembly: AssemblyTitleAttribute("Sync.Today.Util")>]
+[<assembly: AssemblyProductAttribute("SyncToday2015R2")>]
+[<assembly: AssemblyDescriptionAttribute("Business processes automation platform")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[<assembly: ComVisible(false)>]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("2c594a55-7245-470b-9040-1cb8187824d5")>]
-
-// Version information for an assembly consists of the following four values:
-// 
-//       Major Version
-//       Minor Version 
-//       Build Number
-//       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
-
-do
-    ()
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
